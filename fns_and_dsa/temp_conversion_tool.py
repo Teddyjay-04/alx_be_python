cbb901c1c23abe230838d1903b324188741dc843
Teddyjay-04 <jamesteddy411@gmail.com>
2025-01-12 23:55:11 +0000

    44

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,12 +1,12 @@
 # Conversion factors
-FAHRENHEIT_TO_CELSIUS = 5/9
-CELSIUS_TO_FAHRENHEIT = 9/5
+FAHRENHEIT_TO_CELSIUS_FACTOR= 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-    return (celsius * CELSIUS_TO_FAHRENHEIT) + 32
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 def main():
     while True:
