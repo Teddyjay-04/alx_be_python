d50538ac8352f1c64d6a6d7f39571d0039f4b998
Teddyjay-04 <jamesteddy411@gmail.com>
2025-01-26 02:17:45 +0000

    Add Book class with magic methods and test script

---
+++ oop/book_class.py
@@ -0,0 +1,18 @@
+class Book:
+    def __init__(self, title: str, author: str, year: int):
+        """Initialize a Book instance with title, author, and year."""
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        """Destructor that prints a message when a Book instance is deleted."""
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        """Return a string representation of the Book instance."""
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        """Return an official string representation of the Book instance."""
+        return f"Book('{self.title}', '{self.author}', {self.year})"
